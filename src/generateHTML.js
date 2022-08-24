f2739c03a208fd8c4872ea52a4c62d988e86f169
Benjamin Bushman <benbushman98@gmail.com>
2022-08-24 06:07:45 -0600

    worked on the generate HTML page

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -1,6 +1,37 @@
+const createdTeam = (theTeam) => {
+    const genTeam = [];
 
+    const getIntern = intern => {
+        // let internSection = 
+        genTeam.push(interSection);
+    }
 
-function generateHTML() {
+    const getEngineer = engineer => {
+        // let engineerSection =
+        genTeam.push(engineerSection);
+    }
+
+    const getManager = manager => {
+        // let managerSection = 
+        genTeam.push(managerSection);
+    }
+
+    for (employee of theTeam) {
+        if (employee.getRole() === 'Intern') {
+            getIntern(employee)
+        }
+        if (employee.getRole() === 'Engineer') {
+            getEngineer(employee)
+        }
+        if (employee.getRole() === 'Manager') {
+            getManager(employee)
+        }
+    }
+
+    return genTeam.join('')
+}
+
+function generateHTML(yourHTML) {
 
 }
 
